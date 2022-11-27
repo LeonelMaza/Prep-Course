a509e43244f3358c085f11205758c7c2b6b67acd
LeonelMaza <leonel.maza@hotmail.com>
2022-11-27 20:36:50 -0300

    Second commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -72,7 +72,7 @@ function tienenMismaLongitud(str1, str2) {
   // De lo contrario, devuelve "false"
   // Tu código:
   if (str1.length == str2.length) {
-    retrun true;
+    return true;
   } else {
     return false;
   }
